7cedbf4655f8ac17e0d98169431dc0b8b59a5c44
Alex Coaton <alex.coaton@gmail.com>
2020-06-11 21:21:54 +0100

    PlayerPlatform now inherits from Sprite, like Block and Projectile.

--- source/playerplatform.py
+++ source/playerplatform.py
@@ -1,29 +1,26 @@
 import operator
-
 import pygame
 
-from cartesiantypes import Point
 from projectile import Projectile
 from playercontroller import Movement
 
 
-class PlayerPlatform:
+class PlayerPlatform(pygame.sprite.Sprite):
     platform_sprites = {'small': 'SmallPlatform.png', 'medium': 'MediumPlatform.png', 'large': 'LargePlatform.png'}
     BOTTOM_SPACING = 80
     MOVEMENT_SPEED = 900
 
     def __init__(self, screen, sprite_sheet):
+        pygame.sprite.Sprite.__init__(self)
         self.screen = screen
         self.sprite_sheet = sprite_sheet
-        self.position = Point(screen.get_rect().midbottom[0], screen.get_rect().midbottom[1] -
-                              self.BOTTOM_SPACING)
         self.image = self.sprite_sheet.image_by_name(self.platform_sprites['small'])
+        self.rect = self.image.get_rect()
+        self.rect.center = (screen.get_rect().midbottom[0], screen.get_rect().midbottom[1] -
+                            self.BOTTOM_SPACING)
         self.projectile = Projectile(self.screen, self.sprite_sheet, Projectile.types["small"])
         self.projectile_sprite_group = pygame.sprite.Group()
         self.projectile_sprite_group.add(self.projectile)
-
-    def get_position(self):
-        return self.position
 
     def move(self, movement, delta_t):
         distance = self.MOVEMENT_SPEED * delta_t / 1000
@@ -34,11 +31,10 @@ class PlayerPlatform:
         else:
             distance = 0
 
-        self.position = tuple(map(operator.add, self.position, (distance, 0)))
+        self.rect.center = tuple(map(operator.add, self.rect.center, (distance, 0)))
 
         if not self.projectile.is_in_flight():
-            projectile_size = self.projectile.get_size()
-            projectile_position = Point(self.position[0], self.position[1] - projectile_size[0])
+            projectile_position = (self.rect.centerx, self.rect.centery - self.projectile.rect.w)
             self.projectile.update_launch_angle(movement)
             self.projectile.set_position(projectile_position)
 
@@ -46,7 +42,5 @@ class PlayerPlatform:
         self.projectile.fire()
 
     def update(self, delta_t, blocks_group):
-        rectangle = self.image.get_rect()
-        rectangle.center = self.position
-        self.screen.blit(self.image, rectangle)
+        self.screen.blit(self.image, self.rect)
         self.projectile.update(delta_t, blocks_group)
